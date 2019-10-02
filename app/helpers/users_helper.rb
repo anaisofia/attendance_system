659185441467db07ebed6acafcd6e4bbc8b5c8a3
Anaisofia <anaisrivas.26@gmail.com>
2019-10-02 19:08:30 -0300

    recuperando el proyecto con versión anterior, debo trabajar en las asociaciones de usuarios y autorizaciones

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,19 @@
+module UsersHelper
+#   def display_teachers(teachers)
+#     list = "<ol>"
+#     teachers.each do |teacher|
+#       list += "<li> #{teacher} </li>"
+#     end
+#     list += "</ol>"
+#     return list.html_safe
+#   end
+#
+#   def display_students(students)
+#     list = "<ol>"
+#     students.each do |student|
+#       list += "<li> #{student} </li>"
+#     end
+#     list += "</ol>"
+#     return list.html_safe
+#   end
+end
